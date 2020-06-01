fffd940577e7b98062db88985c4aa2c666e19ad8
Nikolas Kososik <nikolas.kososik@gmail.com>
2020-06-01 16:29:24 -0700

    update prompt to return capital letter at start of string
    Co-authored-by: Johnny Duverseau <duverseaujohnny21@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+// const string = prompt("Enter a sentence!");
+
+// // console.log(sentence);
+
+// const input = function(string) {
+//   // return string.charAt(0).toUpperCase + string.charAt(str.length-1);
+//   return string.toUpperCase;
+// };
+
+// const result = 
+// alert(input);
+
+const add = function(string) {
+  return string;
+};
+const string = prompt("Enter a sentence:");
+const nameString = string.charAt(0).toUpperCase() + string.slice(1);
+
+const result = add(nameString);
+alert(result);
